d306a655d44afdf3f20aead91e3cba00231f521b
SIVA PRASAD I K <82268712+sprasadik2010@users.noreply.github.com>
2022-11-14 17:32:54 +0530

    Update getcolors.py

--- getcolors.py
+++ getcolors.py
@@ -3,6 +3,8 @@ import cv2
 import numpy
 import numpy as np
 # Reading the image
-img = cv2.imread('image.jpg')
-colors = img
-print(img)
+img = cv2.imread('image.jpg',1)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+print(hsv)
+cv2.imshow("Output", hsv)
+cv2.waitKey(0)

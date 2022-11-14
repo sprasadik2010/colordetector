b61124874ed9127264ddfe976ca9382c7012568c
sprasadik2010 <82268712+sprasadik2010@users.noreply.github.com>
2022-11-14 15:41:52 +0530

    first commit

---
+++ getcolors.py
@@ -0,0 +1,8 @@
+# TechVidvan Object detection of similar color
+import cv2
+import numpy
+import numpy as np
+# Reading the image
+img = cv2.imread('image.jpg')
+colors = img
+print(img)
---
+++ objectcounter.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('dresses.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (11, 11), 0)
+canny = cv2.Canny(blur, 30, 150, 3)
+dilated = cv2.dilate(canny, (1, 1), iterations=0)
+
+(cnt, hierarchy) = cv2.findContours(
+    dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+output = cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
+cv2.imshow("Output",output)
+cv2.waitKey(0)
+print("dresses in the image : ", len(cnt))

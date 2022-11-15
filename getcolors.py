6cfb7c4535aabfe80d3dfcdbdc1f4ebbc29e8ee5
SIVA PRASAD I K <82268712+sprasadik2010@users.noreply.github.com>
2022-11-15 15:39:53 +0530

    Update getcolors.py

--- getcolors.py
+++ getcolors.py
@@ -1,10 +1,14 @@
-# TechVidvan Object detection of similar color
+
 import cv2
 import numpy
 import numpy as np
 # Reading the image
 img = cv2.imread('image.jpg',1)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-print(hsv)
-cv2.imshow("Output", hsv)
-cv2.waitKey(0)
+hsvs = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsvarray = []
+for hsv in hsvs:
+    for item in hsv:
+        hsvarray.append(item[2])
+
+uniquehsaarray = [*set(hsvarray)]
+print(uniquehsaarray)
